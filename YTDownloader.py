0389c4e236d999bea2d1bae01b2729304a3db740
Felipe Ribeiro <felipe.ribeiro@hellosaks.com>
2022-12-24 18:42:34 -0300

    feat: refactor path

--- YTDownloader.py
+++ YTDownloader.py
@@ -2,67 +2,69 @@ import os
 from pytube import YouTube
 from pytube.cli import on_progress
 from pytube import Playlist
-from moviepy.editor import * 
+from moviepy.editor import *
+
+PATH = '/home/felipe/Música/'
+
 
 def get_video(yt):
-    stream = yt.streams.filter(file_extension='mp4', progressive=True, type='video')
+    stream = yt.streams.filter(
+        file_extension='mp4', progressive=True, type='video')
     video = stream.get_highest_resolution()
     return video
 
+
 def converte(video):
     nome = video.title
-    if(nome.find("'") != -1):
+    if (nome.find("'") != -1):
         nome = nome.replace("'", "")
-    
-    if(nome.find("/") != -1):
+
+    if (nome.find("/") != -1):
         nome = nome.replace("/", "")
-    
-    if(nome.find("?") != -1):
+
+    if (nome.find("?") != -1):
         nome = nome.replace("?", "")
 
-    if(nome.find(":") != -1):
+    if (nome.find(":") != -1):
         nome = nome.replace(":", "")
 
-    if(nome.find(".") != -1):
+    if (nome.find(".") != -1):
         nome = nome.replace(".", "")
-        
-    if(nome.find(",") != -1):
-        nome = nome.replace(",", "")
-        
 
-    if(os.path.exists('/home/felipe/YTDownloader/' + nome + '.mp4')):
-        videoclip = VideoFileClip('/home/felipe/YTDownloader/' + nome + '.mp4')
+    if (nome.find(",") != -1):
+        nome = nome.replace(",", "")
+
+    if (os.path.exists(PATH + nome + '.mp4')):
+        videoclip = VideoFileClip(PATH + nome + '.mp4')
         audioclip = videoclip.audio
-        audioclip.write_audiofile('/home/felipe/YTDownloader/' + nome + '.mp3')
+        audioclip.write_audiofile(PATH + nome + '.mp3')
         videoclip.close()
         audioclip.close()
-        os.remove('/home/felipe/YTDownloader/' + nome + '.mp4')
-    
+        os.remove(PATH + nome + '.mp4')
+
     else:
         print(nome, ' : arquivo não encontrado.')
 
 
-while(True):
+while (True):
     url = input('Link do vídeo: ')
-    
-    if(url == 'close'):
+
+    if (url == 'close'):
         quit()
-    
+
     controle = int(input('Formato (Áudio[1] Vídeo[2]): '))
 
-    if(url.find('/playlist') != -1 or url.find('&list') != -1):
+    if (url.find('/playlist') != -1 or url.find('&list') != -1):
         pl = Playlist(url)
         print('Playlist: s')
-        
+
         for i in pl.videos:
             i.register_on_progress_callback(on_progress)
             i = get_video(i)
-            i.download('/home/felipe/YTDownloader')
+            i.download(PATH)
 
-            if(controle == 1):
+            if (controle == 1):
                 converte(i)
-
-
 
     else:
         yt = YouTube(url, on_progress_callback=on_progress)
@@ -70,15 +72,9 @@ while(True):
         print("Playlist: n")
 
         video = get_video(yt)
-        video.download('/home/felipe/YTDownloader')
+        video.download(PATH)
 
-        if(controle == 1):
+        if (controle == 1):
             converte(video)
 
     print('\n')
-        
-
-
-
-
-
